eeba04d9b1abfcc8a0fa7b1aaf26aa808e9110f4
sahuadarsh0 <sahuadarsh0@gmail.com>
2021-08-25 11:00:47 +0530

    added required dependencies

--- app/src/main/java/technited/minds/pokemon/MainActivity.kt
+++ app/src/main/java/technited/minds/pokemon/MainActivity.kt
@@ -2,7 +2,9 @@ package technited.minds.pokemon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/technited/minds/pokemon/MyApp.kt
@@ -0,0 +1,9 @@
+package technited.minds.pokemon
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApp : Application() {
+
+}
